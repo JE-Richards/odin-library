73342ddc3e3739089e266041a1926f80a7343b44
Jon-Ethan Richards <45516368+JE-Richards@users.noreply.github.com>
2024-06-18 14:52:23 +0100

    Refactor object contructors to classes
    The following changes have been implemented:
    - Refactored the Book object constructor into an object class.
    - Moved the addBookToLibrary, toggleRead, and displayBook functions into
    the class as static methods.
    - The underlying library array (myLibrary) has also been moved into the
    Book class. Originally, myLibrary had been declared as a global variable
    but by putting it as a private static array inside the Book class, it
    safeguards it from external manipulation.
    - Moved the entire script into a factory function. This factory function
    is immediately called and returns nothing. This ensures that the UI is
    the only thing that can manipulate the library array.

--- script.js
+++ script.js
@@ -1,111 +1,112 @@
-const myLibrary = [];
+function PersonalLibrary() {
+    class Book {
 
-// define the book object to be used to create library entries
-function Book (title, author, pageCount, readStatus, rating) {
-    this.title = title;
-    this.author = author;
-    this.pageCount = pageCount;
-    this.readStatus = readStatus;
-    this.rating = rating;
-}
+        static #myLibrary = [];
 
-Book.prototype.addBookToLibrary = function() {
-    return myLibrary.push(this);
-}
-
-Book.prototype.toggleRead = function() {
-    if (this.readStatus === false) {
-        this.readStatus = true;
-    }
-    else if (this.readStatus === true) {
-        this.readStatus = false;
-    }
-}
-
-// function to be called when the form is submitted
-// takes the last entry in the myLibrary array and creates a corresponding table entry for it
-function displayBooks () {
-    const tbody = document.querySelector('tbody');
-
-    let row = tbody.insertRow(-1);
-    row.dataset.book = myLibrary.at(-1)['title'];
-    let tableCols = ['title', 'author', 'pageCount', 'readStatus', 'rating', 'action'];
-
-    for (let i=0; i < tableCols.length; i++) {
-        let cell = row.insertCell(i);
-        if (tableCols[i] === 'title' ||
-            tableCols[i] === 'author' ||
-            tableCols[i] === 'pageCount' ||
-            tableCols[i] === 'rating' 
-        ) {
-            cell.innerHTML = myLibrary.at(-1)[tableCols[i]];
+        constructor (title, author, pageCount, readStatus, rating) {
+            this.title = title;
+            this.author = author;
+            this.pageCount = pageCount;
+            this.readStatus = readStatus;
+            this.rating = rating;
         }
-        else if (tableCols[i] === 'readStatus') {
-            const checkbox = document.createElement('input');
-            checkbox.type = 'checkbox';
-            checkbox.id = 'bookReadStatus';
-            checkbox.checked = myLibrary.at(-1)[tableCols[i]]
-            checkbox.addEventListener('change', (event) => {
-                let checkRow = event.target.closest('tr');
-                let bookTitle = checkRow.getAttribute('data-book'); 
-                let bookIndex = myLibrary.findIndex(item => item.title === bookTitle);
-                myLibrary[bookIndex].toggleRead();
-            })
-            
-            cell.appendChild(checkbox);
+
+        static addBookToLibrary(book) {
+            Book.#myLibrary.push(book);
         }
-        else if (tableCols[i] === 'action') {
-            cell.innerHTML = '<button class="deleteBtn">Delete</button>';
 
-            // Couldn't get the delte function to work correctly when I recreated it as it's own global function
-            row.querySelector('.deleteBtn').addEventListener('click', (event) => {
-                const delRow = event.target.closest('tr');
-                const delTitle = delRow.getAttribute('data-book');
-                
-                // delete row in table
-                delRow.remove();
-
-                // Find and delete the corresponding array entry
-                let delIndex = myLibrary.findIndex(item => item.title === delTitle);
-                myLibrary.splice(delIndex, 1);
-            });
+        static toggleRead(book) {
+        (book.readStatus === true) ? (book.readStatus = false) : (book.readStatus = true);
         }
-    }
-}
 
-const addBookBtn = document.getElementById("addBook");
-const dialog = document.querySelector('dialog');
-const inputForm = document.getElementById("userInput");
-const cancelBtn = document.getElementById("cancel");
+        // function to be called when the form is submitted
+        // takes the last entry in the myLibrary array and creates a corresponding table entry for it
+        static displayBooks() {
+            const tbody = document.querySelector('tbody');
 
-// Add functionality to the form and corresponding buttons
-addBookBtn.addEventListener("click", () => {
-    dialog.showModal();
-});
+            let row = tbody.insertRow(-1);
+            row.dataset.book = Book.#myLibrary.at(-1)['title'];
+            let tableCols = ['title', 'author', 'pageCount', 'readStatus', 'rating', 'action'];
 
-inputForm.addEventListener("submit", (event) => {
-    event.preventDefault();
+            for (let i=0; i < tableCols.length; i++) {
+                let cell = row.insertCell(i);
+                if (tableCols[i] === 'title' ||
+                    tableCols[i] === 'author' ||
+                    tableCols[i] === 'pageCount' ||
+                    tableCols[i] === 'rating' 
+                ) {
+                    cell.innerHTML = Book.#myLibrary.at(-1)[tableCols[i]];
+                }
+                else if (tableCols[i] === 'readStatus') {
+                    const checkbox = document.createElement('input');
+                    checkbox.type = 'checkbox';
+                    checkbox.id = 'bookReadStatus';
+                    checkbox.checked = Book.#myLibrary.at(-1)[tableCols[i]]
+                    checkbox.addEventListener('change', (event) => {
+                        let checkRow = event.target.closest('tr');
+                        let bookTitle = checkRow.getAttribute('data-book'); 
+                        let bookIndex = Book.#myLibrary.findIndex(item => item.title === bookTitle);
+                        Book.toggleRead(Book.#myLibrary[bookIndex]);
+                    })
+                    
+                    cell.appendChild(checkbox);
+                }
+                else if (tableCols[i] === 'action') {
+                    cell.innerHTML = '<button class="deleteBtn">Delete</button>';
 
-    const title = document.getElementById('title').value;
-    const author = document.getElementById('author').value;
-    const pageCount = document.getElementById('pageCount').value;
-    const readStatus = document.getElementById('readStatus').checked;
-    let rating = null;
+                    // Couldn't get the delte function to work correctly when I recreated it as it's own global function
+                    row.querySelector('.deleteBtn').addEventListener('click', (event) => {
+                        const delRow = event.target.closest('tr');
+                        const delTitle = delRow.getAttribute('data-book');
+                        
+                        // delete row in table
+                        delRow.remove();
 
-    const stars = document.getElementsByName('stars');
-    for (i=0; i < stars.length; i++) {
-        if (stars[i].checked) {
-            rating = stars[i].value;
+                        // Find and delete the corresponding array entry
+                        let delIndex = Book.#myLibrary.findIndex(item => item.title === delTitle);
+                        Book.#myLibrary.splice(delIndex, 1);
+                    });
+                }
+            }
         }
     }
 
+    const addBookBtn = document.getElementById("addBook");
+    const dialog = document.querySelector('dialog');
+    const inputForm = document.getElementById("userInput");
+    const cancelBtn = document.getElementById("cancel");
 
-    displayBooks((new Book(title, author, pageCount, readStatus, rating)).addBookToLibrary());
+    // Add functionality to the form and corresponding buttons
+    addBookBtn.addEventListener("click", () => {
+        dialog.showModal();
+    });
 
-    event.target.reset();
-    dialog.close();
-})
+    inputForm.addEventListener("submit", (event) => {
+        event.preventDefault();
 
-cancelBtn.addEventListener("click", () => {
-    dialog.close();
-});
+        const title = document.getElementById('title').value;
+        const author = document.getElementById('author').value;
+        const pageCount = document.getElementById('pageCount').value;
+        const readStatus = document.getElementById('readStatus').checked;
+        let rating = null;
+
+        const stars = document.getElementsByName('stars');
+        for (i=0; i < stars.length; i++) {
+            if (stars[i].checked) {
+                rating = stars[i].value;
+            }
+        }
+
+
+        Book.displayBooks(Book.addBookToLibrary(new Book(title, author, pageCount, readStatus, rating)));
+
+        event.target.reset();
+        dialog.close();
+    })
+
+    cancelBtn.addEventListener("click", () => {
+        dialog.close();
+    });
+}
+
+PersonalLibrary();
